010565be6531fa7071c636d75e009ced4b4f2528
Chris Muga <chrismuga94@gmail.com>
2018-12-28 20:06:40 +0300

    login, working with url patterns

--- exams_python/urls.py
+++ exams_python/urls.py
@@ -20,6 +20,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('exams/', include('exams.urls')),
+    path('', include('exams.urls')),
     
 ]
