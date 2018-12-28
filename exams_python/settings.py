4d4f9eb637db34ccf69007b4ddff1e85156b0165
Chris Muga <chrismuga94@gmail.com>
2018-12-28 19:44:31 +0300

    partials

--- exams_python/settings.py
+++ exams_python/settings.py
@@ -54,7 +54,9 @@ ROOT_URLCONF = 'exams_python.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            'exams/templates'
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
